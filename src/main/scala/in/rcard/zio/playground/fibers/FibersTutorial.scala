992b7608df0c295f34e3a61e28dde02c20e023a9
rcardin <riccardo.cardin@gmail.com>
2021-06-19 15:39:07 +0200

    Added the example on a fiber's interruption

--- src/main/scala/in/rcard/zio/playground/fibers/FibersTutorial.scala
+++ src/main/scala/in/rcard/zio/playground/fibers/FibersTutorial.scala
@@ -1,6 +1,8 @@
 package in.rcard.zio.playground.fibers
 
 import zio._
+import zio.clock.Clock
+import zio.duration.durationInt
 
 object FibersTutorial extends zio.App {
 
@@ -25,10 +27,23 @@ object FibersTutorial extends zio.App {
     bathFiber <- bathTime.debug(printThread).fork
     boilingFiber <- boilingWater.debug(printThread).fork
     zippedFiber = bathFiber.zip(boilingFiber)
-    _ <- zippedFiber.join.debug(printThread)
-    _ <- preparingCoffee.debug(printThread)
+    result <- zippedFiber.join.debug(printThread)
+    _ <- ZIO.succeed(s"$result...done").debug(printThread) *> preparingCoffee.debug(printThread)
+  } yield ()
+
+  val aliceCalling = ZIO.succeed("Alice's call")
+  val boilingWaterWithSleep =
+    boilingWater.debug(printThread) *>
+      ZIO.sleep(5.seconds) *>
+      ZIO.succeed("Boiled water ready")
+
+  def concurrentWakeUpRoutineWithAliceCall(): ZIO[Clock, Nothing, Unit] = for {
+    _ <- bathTime.debug(printThread)
+    boilingFiber <- boilingWaterWithSleep.fork
+    _ <- aliceCalling.debug(printThread).fork *> boilingFiber.interrupt.debug(printThread)
+    _ <- ZIO.succeed("Going to the Cafe with Alice").debug(printThread)
   } yield ()
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
-    concurrentWakeUpRoutine().exitCode
+    concurrentWakeUpRoutineWithAliceCall().exitCode
 }
