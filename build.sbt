75e58323932a7458e867286112761b7a35f23bb7
rcardin <riccardo.cardin@gmail.com>
2021-07-05 22:04:15 +0200

    Added some very basic examples on ZIO streams

--- build.sbt
+++ build.sbt
@@ -5,3 +5,4 @@ version := "0.1"
 scalaVersion := "2.13.6"
 
 libraryDependencies += "dev.zio" %% "zio" % "1.0.9"
+libraryDependencies += "dev.zio" %% "zio-streams" % "1.0.9"
---
+++ src/main/scala/in/rcard/zio/playground/streams/Streams.scala
@@ -0,0 +1,32 @@
+package in.rcard.zio.playground.streams
+
+import zio._
+import zio.console.Console
+import zio.{Chunk, ZIO}
+import zio.stream.ZStream
+
+import java.io.IOException
+
+object Streams {
+
+  val streamFromList: ZStream[Any, Nothing, Int] = ZStream.fromIterable(List(1, 2, 3, 4, 5, 6))
+  val streamUsingApply: ZStream[Any, Nothing, Int] = ZStream(1, 2, 3, 4, 5, 6)
+
+  val steamFromEffect: ZStream[Any, Nothing, Int] =
+    ZStream.fromEffect(ZIO.succeed(42)) ++
+    ZStream.fromEffect(ZIO.succeed(43))
+
+  val streamFromRepeat: ZStream[Any, Nothing, Int] = ZStream.repeatEffect(ZIO.succeed(42))
+
+  trait Tweet
+  lazy val getTweets: ZIO[Any, Nothing, Chunk[Tweet]] = ???
+
+  val streamOfTweets: ZStream[Any, Nothing, Tweet] = ZStream.repeatEffectChunk(getTweets)
+
+  val zioFromDrainingStream: ZIO[Any, Nothing, Unit] = streamOfTweets.runDrain
+
+  val foreachWithStreams: ZIO[Console, IOException, Unit] = for {
+    x <- streamFromList
+    y <- streamFromRepeat
+  } console.putStrLn((x, y).toString())
+}
