9ec5571c2a57a2bb04564aed9fc83b0874d28f13
rcardin <riccardo.cardin@gmail.com>
2021-07-06 22:05:17 +0200

    Added the creation of the first consumer with ZIO Kafka

--- src/main/scala/in/rcard/zio/playground/streams/ZioKafka.scala
+++ src/main/scala/in/rcard/zio/playground/streams/ZioKafka.scala
@@ -1,5 +1,29 @@
 package in.rcard.zio.playground.streams
 
-object ZioKafka {
+import zio.blocking.Blocking
+import zio.clock.Clock
+import zio.kafka.consumer.{Consumer, ConsumerSettings, Subscription}
+import zio.kafka.serde.Serde
+import zio.{ExitCode, Has, RManaged, URIO, ZLayer}
 
+object ZioKafka extends zio.App {
+
+  case class Stock(name: String, price: Double)
+
+  val consumerSettings: ConsumerSettings =
+    ConsumerSettings(List("localhost:9092"))
+      .withGroupId("stocks-consumer")
+
+  val managedConsumer: RManaged[Clock with Blocking, Consumer.Service] =
+    Consumer.make(consumerSettings)
+
+  val consumer: ZLayer[Clock with Blocking, Throwable, Has[Consumer.Service]] =
+    ZLayer.fromManaged(managedConsumer)
+
+  Consumer.subscribeAnd(Subscription.topics("stocks"))
+    .plainStream(Serde.string, Serde.double)
+    .map(record => Stock(record.key, record.value))
+//    .map()
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = ???
 }
