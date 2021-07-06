492adb1063404d17c60ae1c5c2d2be11d06d0a39
rcardin <riccardo.cardin@gmail.com>
2021-07-06 22:04:41 +0200

    Added a zio.App to the Streams examples

--- src/main/scala/in/rcard/zio/playground/streams/Streams.scala
+++ src/main/scala/in/rcard/zio/playground/streams/Streams.scala
@@ -7,7 +7,7 @@ import zio.stream.ZStream
 
 import java.io.IOException
 
-object Streams {
+object Streams extends zio.App {
 
   val streamFromList: ZStream[Any, Nothing, Int] = ZStream.fromIterable(List(1, 2, 3, 4, 5, 6))
   val streamUsingApply: ZStream[Any, Nothing, Int] = ZStream(1, 2, 3, 4, 5, 6)
@@ -18,15 +18,22 @@ object Streams {
 
   val streamFromRepeat: ZStream[Any, Nothing, Int] = ZStream.repeatEffect(ZIO.succeed(42))
 
-  trait Tweet
-  lazy val getTweets: ZIO[Any, Nothing, Chunk[Tweet]] = ???
+//  trait Tweet
+//  lazy val getTweets: ZIO[Any, Nothing, Chunk[Tweet]] = ???
+//
+//  val streamOfTweets: ZStream[Any, Nothing, Tweet] = ZStream.repeatEffectChunk(getTweets)
 
-  val streamOfTweets: ZStream[Any, Nothing, Tweet] = ZStream.repeatEffectChunk(getTweets)
-
-  val zioFromDrainingStream: ZIO[Any, Nothing, Unit] = streamOfTweets.runDrain
+//  val zioFromDrainingStream: ZIO[Any, Nothing, Unit] = streamOfTweets.runDrain
 
   val foreachWithStreams: ZIO[Console, IOException, Unit] = for {
     x <- streamFromList
     y <- streamFromRepeat
   } console.putStrLn((x, y).toString())
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
+    streamFromList
+      .mapM(n => console.putStrLn(n.toString))
+      .runDrain
+      .exitCode
+  }
 }
