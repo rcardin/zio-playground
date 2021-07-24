fd75098f6afd1819ea31787ebd996549d890e8ee
rcardin <riccardo.cardin@gmail.com>
2021-07-24 15:57:56 +0200

    Added the Serde for the Players type

--- src/main/scala/in/rcard/zio/playground/streams/ZioKafka.scala
+++ src/main/scala/in/rcard/zio/playground/streams/ZioKafka.scala
@@ -2,12 +2,11 @@ package in.rcard.zio.playground.streams
 
 import zio.blocking.Blocking
 import zio.clock.Clock
-import zio.console
+import zio.{ExitCode, Has, RManaged, URIO, ZIO, ZLayer, console}
 import zio.console.Console
-import zio.kafka.consumer.{CommittableRecord, Consumer, ConsumerSettings, Subscription}
+import zio.kafka.consumer.{CommittableRecord, Consumer, ConsumerSettings, SubscribedConsumerFromEnvironment, Subscription}
 import zio.kafka.serde.Serde
 import zio.stream.ZStream
-import zio.{ExitCode, Has, RManaged, URIO, ZLayer}
 
 // Commands for Kafka broker
 //
@@ -27,7 +26,23 @@ import zio.{ExitCode, Has, RManaged, URIO, ZLayer}
 object ZioKafka extends zio.App {
 
   // {ITA-ENG, 1-1}
+  case class Players(p1: String, p2: String)
+
   case class Match(players: String, score: String)
+
+  val playersSerde: Serde[Any, Players] = Serde.string.inmapM { playersAsString =>
+    ZIO.effect {
+      if (!playersAsString.matches("...-...")) {
+        throw new IllegalArgumentException(s"$playersAsString doesn't represents two players")
+      }
+      val split = playersAsString.split("-")
+      Players(split(0), split(1))
+    }
+  } { players =>
+    ZIO.succeed {
+      s"${players.p1}-${players.p2}"
+    }
+  }
 
   val consumerSettings: ConsumerSettings =
     ConsumerSettings(List("localhost:9092"))
@@ -43,13 +58,11 @@ object ZioKafka extends zio.App {
     Consumer.subscribeAnd(Subscription.topics("updates"))
       .plainStream(Serde.string, Serde.string)
 
-  val itaMatchesStreams =
+  val itaMatchesStreams: SubscribedConsumerFromEnvironment =
     Consumer.subscribeAnd(Subscription.pattern("updates|.*ITA.*".r))
-      .plainStream(Serde.string, Serde.string)
 
-  val partitionedMatchesStreams =
+  val partitionedMatchesStreams: SubscribedConsumerFromEnvironment =
     Consumer.subscribeAnd(Subscription.manual("updates", 1))
-      .plainStream(Serde.string, Serde.string)
 
   val stream: ZStream[Console with Consumer with Clock, Throwable, Unit] =
     Consumer.subscribeAnd(Subscription.topics("updates"))
