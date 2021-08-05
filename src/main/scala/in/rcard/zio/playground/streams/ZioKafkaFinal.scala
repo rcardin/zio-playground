626a776366332ee7c086bc380f69c0f0afe4cb5f
rcardin <riccardo.cardin@gmail.com>
2021-08-05 22:05:59 +0200

    Added the object with the example of the article

---
+++ src/main/scala/in/rcard/zio/playground/streams/ZioKafkaFinal.scala
@@ -0,0 +1,74 @@
+package in.rcard.zio.playground.streams
+
+import zio._
+import zio.blocking.Blocking
+import zio.clock.Clock
+import zio.console.Console
+import zio.json._
+import zio.kafka.consumer._
+import zio.kafka.serde.Serde
+import zio.stream.ZSink
+
+// Commands for Kafka broker
+//
+// docker exec -it broker bash
+//
+// kafka-topics \
+//   --bootstrap-server localhost:9092 \
+//   --topic updates \
+//   --create
+//
+// kafka-console-producer \
+//   --topic updates \
+//   --broker-list localhost:9092 \
+//   --property parse.key=true \
+//   --property key.separator=,
+//
+object ZioKafkaFinal extends zio.App {
+
+  case class Player(name: String, score: Int) {
+    override def toString: String = s"$name: $score"
+  }
+
+  object Player {
+    implicit val decoder: JsonDecoder[Player] = DeriveJsonDecoder.gen[Player]
+    implicit val encoder: JsonEncoder[Player] = DeriveJsonEncoder.gen[Player]
+  }
+
+  case class Match(players: Array[Player]) {
+    def score: String = s"${players(0)} - ${players(1)}"
+  }
+
+  object Match {
+    implicit val decoder: JsonDecoder[Match] = DeriveJsonDecoder.gen[Match]
+    implicit val encoder: JsonEncoder[Match] = DeriveJsonEncoder.gen[Match]
+  }
+
+  val matchSerde: Serde[Any, Match] = Serde.string.inmapM { matchAsString =>
+    ZIO.fromEither(matchAsString.fromJson[Match].left.map(new RuntimeException(_)))
+  } { matchAsObj =>
+    ZIO.effect(matchAsObj.toJson)
+  }
+
+  val consumerSettings: ConsumerSettings =
+    ConsumerSettings(List("localhost:9092"))
+      .withGroupId("updates-consumer")
+
+  val managedConsumer: RManaged[Clock with Blocking, Consumer.Service] =
+    Consumer.make(consumerSettings)
+
+  val consumer: ZLayer[Clock with Blocking, Throwable, Has[Consumer.Service]] =
+    ZLayer.fromManaged(managedConsumer)
+
+  val matchesStreams: ZIO[Console with Any with Consumer with Clock, Throwable, Unit] =
+    Consumer.subscribeAnd(Subscription.topics("updates"))
+      .plainStream(Serde.uuid, matchSerde)
+      .map(cr => (cr.value.score, cr.offset))
+      .tap { case (score, _) => console.putStrLn(s"| $score |") }
+      .map { case (_, offset) => offset }
+      .aggregateAsync(Consumer.offsetBatches)
+      .run(ZSink.foreach(_.commit))
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    matchesStreams.provideSomeLayer(consumer ++ zio.console.Console.live).exitCode
+}
